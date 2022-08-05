efc048b4ec0d32ac62f0a5fa14d9d4e32ec181d8
Felipe Alves da Silva <felipealvesdasilva552@gmail.com>
2022-08-05 10:13:16 -0300

    ex04_quem_eh_maior_check

--- src/funcoes.rb
+++ src/funcoes.rb
@@ -17,3 +17,13 @@ end
 def achar_posicao(text, char)
   text.index(char)
 end
+
+def quem_eh_maior(n1, n2)
+  if n1 > n2
+    "Número #{n1} é maior do que o número #{n2}"
+  elsif n1 < n2
+    "Número #{n1} é menor do que o número #{n2}"
+  else
+    "Empate"
+  end
+end
